23b6e4730fb46729da3578b98f1a2f4748b12c09
Dennis Kamau <dkamau476@gmail.com>
2022-10-19 02:22:45 -0700

    Adding main.h

---
+++ main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+int _putchar(char);
+int printstring(char *);
+void writeint(unsigned int, int);
+int printint(int);
+int calculate_numbers(unsigned int);
+int printunsigned(unsigned int);
+void writeunsigned(unsigned int, int);
+int _printf(const char *format, ...);
+#endif
