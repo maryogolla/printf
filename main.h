9417c99443e648042cd9cac32c33a401a4a1531b
Dennis Kamau <dkamau476@gmail.com>
2022-10-19 03:20:53 -0700

    Adding writeunsigned.c and modified main.h

--- main.h
+++ main.h
@@ -9,6 +9,8 @@ int _putchar(char);
 int printstring(char *);
 void writeint(unsigned int, int);
 int printint(int);
+int printunsigned(unsigned int);
+void writeunsigned(unsigned int, int);
 int calculate_numbers(unsigned int);
 int _printf(const char *format, ...);
 #endif
