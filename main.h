64ec773957647ea076f70cc7e673261abd73bd80
maryogolla <maryogolla040@gmail.com>
2022-10-20 12:45:13 -0700

    main.h

--- main.h
+++ main.h
@@ -6,14 +6,12 @@
 
 #define UNUSED(x) (void)(x)
 #define BUFF_SIZE 1024
-
 /* FLAGS */
 #define F_MINUS 1
 #define F_PLUS 2
 #define F_ZERO 4
 #define F_HASH 8
 #define F_SPACE 16
-
 /* SIZES */
 #define S_LONG 2
 #define S_SHORT 1
@@ -29,7 +27,6 @@ struct fmt
 	char fmt;
 	int (*fn)(va_list, char[], int, int, int, int);
 };
-
 
 /**
  * typedef struct fmt fmt_t - Struct op
